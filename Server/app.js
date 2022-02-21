ade2fa2ecbedea8b7c16380a819bc96d5f2550b6
Ashish <ashishprakash52433@gmail.com>
2022-02-21 20:41:17 +0530

    Init Express

---
+++ Server/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(request, response){
+    response.send("<h1>Hello Express</h1>")
+})
+
+app.listen(4000, function(){
+    console.log("App Started");
+})
