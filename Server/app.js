22b8b77933433dbc0f44d187b7d0cdd212e78be0
Ashish <ashishprakash52433@gmail.com>
2022-02-23 10:56:26 +0530

    socket connected

--- Server/app.js
+++ Server/app.js
@@ -2,10 +2,26 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function(request, response){
-    response.send("<h1>Hello Express</h1>")
-})
+const http = require('http');
 
-app.listen(4000, function(){
-    console.log("App Started");
+const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+
+const io = new Server(server);
+
+// app.get("/", function(request, response){
+//     response.send("<h1>Hello Express</h1>")
+// })
+
+app.use(express.static("public"));
+
+io.on("connection", function(socket){
+    console.log(socket.id, "socket connected");
+})
+// app.listen(4000, function(){
+//     console.log("App Started");
+// })
+server.listen(4000, function(){
+    console.log("App started working");
 })
